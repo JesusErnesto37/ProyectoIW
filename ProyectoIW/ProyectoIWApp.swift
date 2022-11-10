e0b27eeaa3c0880e9e153e9be0865bf5b48e9551
JavierVillafana <19170704@itculiacan.edu.mx>
2022-11-10 09:10:04 -0700

    Initial Commit

---
+++ ProyectoIW/ProyectoIWApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoIWApp.swift
+//  ProyectoIW
+//
+//  Created by CCDM31 on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoIWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
