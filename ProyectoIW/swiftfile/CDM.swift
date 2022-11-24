5ef8f722e151dbe3ee8384a6ee8385d08654a81d
JavierVillafana <19170704@itculiacan.edu.mx>
2022-11-24 09:55:56 -0700

    ss

--- ProyectoIW/swiftfile/CDM.swift
+++ ProyectoIW/swiftfile/CDM.swift
@@ -20,12 +20,50 @@ class CDM {
                 fatalError("Core data failed to initizliazee \(error.localizedDescription)")
             }
         })
+    }
+        func guardarU(id: Int32, nombre: String, apellido: String, username: String, rolid:Int32){
+            let usuario = Usuarios(context: persistentContainer.viewContext)
+            usuario.id = id
+            usuario.nombre = nombre
+            usuario.apellido = apellido
+            usuario.username = username
+            usuario.rolid = rolid
+            
+            do{
+                try persistentContainer.viewContext.save()
+                print("Usuario guardado")
+            }
+            catch{
+                print("fallo en guardar \(error) ")
+            }
+            
+    }
+    
+    func leerTU() -> [Usuarios]{
+        let fetchRequest : NSFetchRequest<Usuarios> = Producto.fetchRequest()
+        
+        do{
+            return try persistentContainer.viewContext.fetch(fetchRequest)
+        }
+        catch{return[]}
+    }
+    
+    func borrarU(usuario: Usuarios){
+        persistentContainer.viewContext.delete(usuario)
+        
+        do{
+            try persistentContainer.viewContext.save()
+        }catch{
+            persistentContainer.viewContext.rollback()
+            print("Failed to save context \(error.localizedDescription)")
+        }
+        
+        
         
         
         
         
     }
-    
     
     
 }
